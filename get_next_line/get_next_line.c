3b664ebadd299eb290d5d419515f0fe23e656ac5
Yulia Ioffe <yioffe@student.42lisboa.com>
2023-12-04 19:53:44 +0000

    fixed style

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: yioffe <yioffe@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/30 10:25:34 by yioffe            #+#    #+#             */
-/*   Updated: 2023/12/01 11:57:15 by yioffe           ###   ########.fr       */
+/*   Updated: 2023/12/04 18:15:44 by yioffe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,44 +16,62 @@ char	*get_next_line(int fd)
 {
 	static char	buffer[BUFFER_SIZE + 1];
 	char		*line;
-	int			nl_finder;
+	int			new_line;
 
-	nl_finder = 1;
+	new_line = 1;
 	line = NULL;
 	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
 	{
 		ft_bzero(buffer, BUFFER_SIZE + 1);
 		return (NULL);
 	}
-	while (nl_finder && (buffer[0] || read(fd, buffer, BUFFER_SIZE)))
+	while (new_line && (buffer[0] || read(fd, buffer, BUFFER_SIZE)))
 	{
 		line = ft_strjoin(line, buffer);
-		ft_free(&nl_finder, buffer);
+		ft_free(&new_line, buffer);
 		if (line == NULL)
 			return (NULL);
 	}
 	return (line);
 }
 
-/* int main(int argc, char **argv) {
-    int fd;
+/* void test_gnl(int fd, char *expected)
+{
     char *line;
 
-    if (argc == 2) {
+    while ((line = get_next_line(fd)) != NULL)
+    {
+        printf("Line: %s\n", line);
+        free(line);
+    }
+
+    if (expected == NULL)
+        printf("Expected: NULL\n");
+    else
+        printf("Expected: %s\n", expected);
+} */
+
+/* int main(int argc, char **argv)
+{
+    int fd;
+
+    if (argc == 2)
+    {
+        // Read from file
         fd = open(argv[1], O_RDONLY);
-        if (fd == -1) {
+        if (fd == -1)
+        {
             perror("Error opening file");
             return 1;
         }
-    } else {
-        // If no file provided, read from standard input
+    }
+    else
+    {
+        // Read from standard input
         fd = 0;
     }
 
-    while ((line = get_next_line(fd)) != NULL) {
-        printf("%s\n", line);
-        free(line);
-    }
+    test_gnl(fd, NULL);
 
     close(fd);
 
