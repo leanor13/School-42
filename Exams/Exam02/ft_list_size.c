f4e40dd3fdbc67d630df0c1d39ed46fae3bc3b45
leanor13 <leanor@yandex.ru>
2023-12-14 20:22:02 +0000

    home 14-12-2023

---
+++ Exams/Exam02/ft_list_size.c
@@ -0,0 +1,20 @@
+
+typedef struct    s_list
+{
+    struct s_list *next;
+    void          *data;
+}                 t_list;
+
+int	ft_list_size(t_list *begin_list)
+{
+	int	i = 0;
+
+	if (!begin_list)
+		return (i);
+	while (begin_list)
+	{
+		i ++;
+		begin_list = begin_list -> next;
+	}
+	return (i);
+}
