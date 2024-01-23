9144828efc700f309eed1a2ccb9a071470b4737e
leanor13 <yumioffe@gmail.com>
2024-01-23 17:24:04 +0000

    added validation

--- push_swap/srcs/main.c
+++ push_swap/srcs/main.c
@@ -6,11 +6,37 @@
 /*   By: yioffe <yioffe@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/23 16:18:35 by yioffe            #+#    #+#             */
-/*   Updated: 2024/01/23 16:38:42 by yioffe           ###   ########.fr       */
+/*   Updated: 2024/01/23 17:11:22 by yioffe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
+
+int ft_isnum(char *s)
+{
+	while (*s)
+	{
+		if (!ft_isdigit(*s))
+			return (0);
+		else
+			s ++;
+	}
+	return (1);
+}
+
+int	validate_input(char **av, int ac)
+{
+	while (ac >= 2)
+	{
+		if (!ft_isnum(av[ac - 1]))
+		{
+			ft_printf("Invalid input, only digits are allowed");
+			return (0);
+		}
+		ac --;
+	}
+	return (1);
+}
 
 int	main(int ac, char **av)
 {
@@ -19,7 +45,8 @@ int	main(int ac, char **av)
 		ft_printf("needed at least one integer");
 		return (0);
 	}
-	(void) av;
+	if (validate_input(av, ac))
+		ft_printf("input is valid");
 	return (0);
 }
 
