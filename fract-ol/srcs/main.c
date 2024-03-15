e7becdfe5718b8524294e819abb6690372a8c1a4
leanor13 <leanor@yandex.ru>
2024-03-15 11:21:16 +0000

    home 15-03-2024

--- fract-ol/srcs/main.c
+++ fract-ol/srcs/main.c
@@ -6,8 +6,18 @@
 /*   By: yioffe <yioffe@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/14 12:09:05 by yioffe            #+#    #+#             */
-/*   Updated: 2024/03/14 12:09:27 by yioffe           ###   ########.fr       */
+/*   Updated: 2024/03/14 21:19:03 by yioffe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "../includes/fractol.h"
+#include "../includes/fractol.h"
+
+int	main(void)
+{
+	void	*mlx;
+	void	*mlx_win;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	mlx_loop(mlx);
+}
