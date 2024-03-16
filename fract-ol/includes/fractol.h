4c514d730dcb20d637ec53258569702fa71cda9d
leanor13 <leanor@yandex.ru>
2024-03-16 15:30:39 +0000

    mlx study

--- fract-ol/includes/fractol.h
+++ fract-ol/includes/fractol.h
@@ -6,7 +6,7 @@
 /*   By: yioffe <yioffe@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/23 11:27:07 by yioffe            #+#    #+#             */
-/*   Updated: 2024/03/16 12:02:09 by yioffe           ###   ########.fr       */
+/*   Updated: 2024/03/16 15:18:34 by yioffe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,20 @@
 #else
 # include "minilibx-linux/mlx.h"
 #endif
+
+typedef struct	s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
+
+typedef struct point
+{
+	int	x;
+	int	y;
+}				t_point;
 
 
 #endif
--- fract-ol/srcs/main.c
+++ fract-ol/srcs/main.c
@@ -6,34 +6,60 @@
 /*   By: yioffe <yioffe@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/14 12:09:05 by yioffe            #+#    #+#             */
-/*   Updated: 2024/03/16 12:05:17 by yioffe           ###   ########.fr       */
+/*   Updated: 2024/03/16 15:30:15 by yioffe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fractol.h"
 
-typedef struct	s_data {
-	void	*img;
-	char	*addr;
-	int		bits_per_pixel;
-	int		line_length;
-	int		endian;
-}				t_data;
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
+
+void	my_mlx_horizontal_line_put(t_data data, int x_start, int y_start, int len, int color)
+{
+	int	i;
+
+	i = 0;
+	while (i < len)
+	{
+		my_mlx_pixel_put(&data, x_start + i, y_start, color);
+		i ++;
+	}
+}
+
+void	my_mlx_vertical_line_put(t_data data, int x_start, int y_start, int len, int color)
+{
+	int	i;
+
+	i = 0;
+	while (i < len)
+	{
+		my_mlx_pixel_put(&data, x_start, y_start + i, color);
+		i ++;
+	}
+}
 
 int	main(void)
 {
 	void	*mlx;
+	void	*mlx_win;
 	t_data	img;
+	int		color;
 
 	mlx = mlx_init();
-	//mlx_win = mlx_new_window(mlx, 880, 1080, "Hello world!");
-	//mlx_loop(mlx);
-	img.img = mlx_new_image(mlx, 1920, 1080);
-	/*
-	** After creating an image, we can call `mlx_get_data_addr`, we pass
-	** `bits_per_pixel`, `line_length`, and `endian` by reference. These will
-	** then be set accordingly for the *current* data address.
-	*/
+	mlx_win = mlx_new_window(mlx, 500, 500, "Hello world!");
+	img.img = mlx_new_image(mlx, 500, 500);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
 								&img.endian);
+	color =  0x00FF0000;
+	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
+	my_mlx_horizontal_line_put(img, 50, 50, 100, color);
+	my_mlx_vertical_line_put(img, 200, 200, 100, color + 1000);
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	mlx_loop(mlx);
 }
