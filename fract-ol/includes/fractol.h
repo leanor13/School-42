86e7633646d755c338427788c231e1985e3526e5
leanor13 <leanor@yandex.ru>
2024-03-16 10:38:24 +0000

    revert

--- fract-ol/includes/fractol.h
+++ fract-ol/includes/fractol.h
@@ -6,7 +6,7 @@
 /*   By: yioffe <yioffe@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/23 11:27:07 by yioffe            #+#    #+#             */
-/*   Updated: 2024/03/15 17:00:56 by yioffe           ###   ########.fr       */
+/*   Updated: 2024/03/14 12:10:30 by yioffe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,7 @@
 # define FRACTOL_H
 
 # include "libft/libft.h"
-# include "minilibx-linux/mlx.h"
-# include <X11/X.h>
-# include <X11/keysym.h>
+# include <mlx.h>
 
 
 #endif
--- fract-ol/srcs/main.c
+++ fract-ol/srcs/main.c
@@ -6,34 +6,18 @@
 /*   By: yioffe <yioffe@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/14 12:09:05 by yioffe            #+#    #+#             */
-/*   Updated: 2024/03/15 17:26:44 by yioffe           ###   ########.fr       */
+/*   Updated: 2024/03/14 21:19:03 by yioffe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fractol.h"
 
-typedef struct	s_data {
-	void	*img;
-	char	*addr;
-	int		bits_per_pixel;
-	int		line_length;
-	int		endian;
-}				t_data;
-
 int	main(void)
 {
 	void	*mlx;
-	t_data	img;
+	void	*mlx_win;
 
 	mlx = mlx_init();
-	//mlx_win = mlx_new_window(mlx, 880, 1080, "Hello world!");
-	//mlx_loop(mlx);
-	img.img = mlx_new_image(mlx, 1920, 1080);
-	/*
-	** After creating an image, we can call `mlx_get_data_addr`, we pass
-	** `bits_per_pixel`, `line_length`, and `endian` by reference. These will
-	** then be set accordingly for the *current* data address.
-	*/
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
-								&img.endian);
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	mlx_loop(mlx);
 }
