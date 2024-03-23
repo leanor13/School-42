7f49784d1697d60a61efbcc588c6dc06a2d46fc9
leanor13 <yumioffe@gmail.com>
2024-03-23 19:10:45 +0000

    school 23-03-2024

--- fract-ol/srcs/fractal.c
+++ fract-ol/srcs/fractal.c
@@ -6,7 +6,7 @@
 /*   By: yioffe <yioffe@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/18 10:36:03 by yioffe            #+#    #+#             */
-/*   Updated: 2024/03/23 17:25:48 by yioffe           ###   ########.fr       */
+/*   Updated: 2024/03/23 19:09:46 by yioffe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 t_point	mandelbrot_iter(t_point c, t_point c_0)
 {
 	t_point	next;
-
+	
 	next.x = c.x * c.x - c.y * c.y + c_0.x;
 	next.y = 2 * c.x * c.y + c_0.y;
 	return (next);
@@ -74,18 +74,26 @@ t_point	tricorn_iter(t_point c, t_point c_0)
 int	color_pixel(t_fractal *f, t_pixel pixel)
 {
 	int			i;
-	t_point	curr;
-	t_point	c_0;
+	t_point		curr;
+	t_point		c_0;
 	int			color;
+	double		c2;
 
 	i = 0;
 	curr = my_map_pixel(pixel, f->min_bound, f->max_bound, f->pix_max);
+
+	c2 = curr.x * curr.x + curr.y * curr.y;
+	//if (256.0 * c2 * c2 - 96.0 * c2 + 32.0 * curr.x - 3.0 < 0.0) 
+	//	return (map_color_maxiter(i, MAX_ITER, f->min_bound));
+	//if (16.0 * (c2 + 2.0 * curr.x + 1.0) - 1.0 < 0.0) 
+	//	return (map_color_maxiter(i, MAX_ITER, f->min_bound));
+
 	c_0 = curr;
 	while (i < MAX_ITER)
 	{
 		if ((curr.y * curr.y + curr.x * curr.x >= 4))
 			break ;
-		curr = tricorn_iter(curr, c_0);
+		curr = mandelbrot_iter(curr, c_0);
 		i ++;
 	}
 	color = map_color_maxiter(i, MAX_ITER, f->min_bound);
