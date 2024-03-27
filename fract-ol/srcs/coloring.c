c7f3c6cd8fc01d361d40bbd72db5db10f30faf6b
leanor13 <leanor@yandex.ru>
2024-03-27 11:22:26 +0000

    normi

--- fract-ol/srcs/coloring.c
+++ fract-ol/srcs/coloring.c
@@ -6,20 +6,20 @@
 /*   By: yioffe <yioffe@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/18 22:14:41 by yioffe            #+#    #+#             */
-/*   Updated: 2024/03/27 11:19:03 by yioffe           ###   ########.fr       */
+/*   Updated: 2024/03/27 11:22:19 by yioffe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fractol.h"
 
-int clamp(int value, int min, int max) 
+int	clamp(int value, int min, int max)
 {
 	if (value < min)
-		return min;
+		return (min);
 	else if (value > max)
-		return max;
-	else 
-		return value;
+		return (max);
+	else
+		return (value);
 }
 
 int	map_color_maxiter(int iter, int max_iter)
@@ -31,12 +31,12 @@ int	map_color_maxiter(int iter, int max_iter)
 	double	coeff;
 
 	coeff = 1;
-	if (iter == max_iter) 
+	if (iter == max_iter)
 	{
 		r = 0;
 		g = 0;
 		b = 0;
-		return (r << 16) | (g << 8) | b;
+		return ((r << 16) | (g << 8) | b);
 	}
 	else if (iter == 0)
 		coeff = 500;
@@ -56,12 +56,12 @@ int	map_color_striped(int iter, int max_iter)
 	double	coeff;
 
 	coeff = 1;
-	if (iter == max_iter) 
+	if (iter == max_iter)
 	{
 		r = 0;
 		g = 0;
 		b = 0;
-		return (r << 16) | (g << 8) | b;
+		return ((r << 16) | (g << 8) | b);
 	}
 	if (iter % 2 == 0)
 		log_iter = log(iter + 1) / log(max_iter * coeff);
@@ -75,23 +75,23 @@ int	map_color_striped(int iter, int max_iter)
 
 int	map_color_b_w(int iter, int max_iter)
 {
-	int		r;
-	int		g;
-	int		b;
+	int	r;
+	int	g;
+	int	b;
 
-	if (iter == max_iter) 
+	if (iter == max_iter)
 	{
 		r = 0;
 		g = 0;
 		b = 0;
-		return (r << 16) | (g << 8) | b;
+		return ((r << 16) | (g << 8) | b);
 	}
 	else
 	{
 		r = 255;
 		g = 255;
 		b = 255;
-		return (r << 16) | (g << 8) | b;
+		return ((r << 16) | (g << 8) | b);
 	}
 	return ((r << 16) | (g << 8) | b);
 }
