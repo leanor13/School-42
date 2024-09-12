2f9f15f0adfd647f7e15f00e06f2b643cc38b7c5
Yulia Ioffe <yumioffe@gmail.com>
2024-09-12 14:33:08 +0100

    Normi fix (#3)
    fixed norminette

--- Philosophers/philo/srcs/routines.c
+++ Philosophers/philo/srcs/routines.c
@@ -6,79 +6,81 @@
 /*   By: yioffe <yioffe@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/11 19:53:37 by yioffe            #+#    #+#             */
-/*   Updated: 2024/09/11 20:06:41 by yioffe           ###   ########.fr       */
+/*   Updated: 2024/09/12 14:12:12 by yioffe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
 
-void *monitor_routine(void *params) 
+static int	death_check(t_philo *philo, t_config *config)
 {
-	t_config *config;
-	t_philo *philo;
-	struct timeval current_time;
-	int	done_philo;
-	int	eat_counter;
-	long time_since_last_eat;
+	long			time_since_last_eat;
+	struct timeval	current_time;
+
+	pthread_mutex_lock(&philo->mutex_eating);
+	gettimeofday(&current_time, NULL);
+	time_since_last_eat = time_diff_in_ms(philo->last_eat_time, current_time);
+	if (time_since_last_eat > config->time_to_die)
+	{
+		set_config_stop(config, true);
+		philo_print("died", philo);
+		pthread_mutex_unlock(&philo->mutex_eating);
+		return (EXIT_FAILURE);
+	}
+	pthread_mutex_unlock(&philo->mutex_eating);
+	return (EXIT_SUCCESS);
+}
+
+void	*monitor_routine(void *params)
+{
+	t_config	*config;
+	t_philo		*philo;
+	int			done_philo;
 
 	config = (t_config *)params;
-	while (!check_config_stop(config)) 
+	while (!check_config_stop(config))
 	{
 		philo = config->first_philo;
 		done_philo = 0;
-		while (philo) 
+		while (philo)
 		{
-			pthread_mutex_lock(&philo->mutex_eating);
-			gettimeofday(&current_time, NULL);
-			time_since_last_eat = time_diff_in_ms(philo->last_eat_time, current_time);
-			if (time_since_last_eat > config->time_to_die) 
-			{
-				set_config_stop(config, true);
-				philo_print("died", philo);
-				pthread_mutex_unlock(&philo->mutex_eating);
+			if (death_check(philo, config) == EXIT_FAILURE)
 				return (NULL);
-			}
-			pthread_mutex_unlock(&philo->mutex_eating);
-			eat_counter = get_eat_counter(philo);
-			if (config->max_eat_times >= 0 && eat_counter >= config->max_eat_times)
-				done_philo ++;
+			if (config->max_eat_times >= 0
+				&& get_eat_counter(philo) >= config->max_eat_times)
+				done_philo++;
 			philo = philo->next;
 		}
-		if (config->max_eat_times >= 0 && done_philo == config->number_of_philosophers)
-		{
-			set_config_stop(config, true);
-			return (NULL);
-		}
-		usleep(100); 
+		if (config->max_eat_times >= 0
+			&& done_philo == config->number_of_philos)
+			return (set_config_stop(config, true), NULL);
+		usleep(MONITOR_FREQUENSY_US);
 	}
 	return (NULL);
 }
 
-void *philosopher_routine(void *params) {
-	t_philo			*philo;
-	t_config		*config;
-	struct timeval	start_sleep; 
-	struct timeval	end_sleep;
+void	*philosopher_routine(void *params)
+{
+	t_philo		*philo;
+	t_config	*config;
 
 	philo = (t_philo *)params;
 	config = philo->config;
 	if (!config)
 		return (NULL);
-	if (philo->id % 2 && config->number_of_philosophers > 1)
+	if (philo->id % 2 && config->number_of_philos > 1)
 		philo_sleep(config->time_to_eat / 50, config);
 	while (!check_config_stop(config))
 	{
 		if (check_config_stop(config))
-			break;
+			break ;
 		philo_take_forks_and_eat(philo);
 		if (check_config_stop(config))
-			break;
+			break ;
 		philo_print("is sleeping", philo);
-		gettimeofday(&start_sleep, NULL);
 		philo_sleep(config->time_to_sleep, config);
-		gettimeofday(&end_sleep, NULL);
 		if (check_config_stop(config))
-			break;
+			break ;
 		philo_print("is thinking", philo);
 	}
 	return (NULL);
--- Philosophers/philo/srcs/utils.c
+++ Philosophers/philo/srcs/utils.c
@@ -6,17 +6,15 @@
 /*   By: yioffe <yioffe@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/07 19:03:23 by yioffe            #+#    #+#             */
-/*   Updated: 2024/09/11 19:56:28 by yioffe           ###   ########.fr       */
+/*   Updated: 2024/09/12 14:16:48 by yioffe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
 
-#include <limits.h>
-
 int	atoi_positive(char *str)
 {
-	int		number;
+	int	number;
 
 	if (!str || !*str)
 		return (NEG_ERROR);
@@ -45,9 +43,14 @@ void	ft_putnbr_fd(unsigned long long n, int fd)
 	write(fd, &c, 1);
 }
 
-long time_diff_in_ms(struct timeval start, struct timeval end)
+long	time_diff_in_ms(struct timeval start, struct timeval end)
 {
-    return ((end.tv_sec - start.tv_sec) * 1000) + ((end.tv_usec - start.tv_usec) / 1000);
+	long	sec_diff;
+	long	usec_diff;
+
+	sec_diff = (end.tv_sec - start.tv_sec) * 1000;
+	usec_diff = (end.tv_usec - start.tv_usec) / 1000;
+	return (sec_diff + usec_diff);
 }
 
 long	current_time_in_ms(void)
@@ -55,5 +58,29 @@ long	current_time_in_ms(void)
 	struct timeval	time;
 
 	gettimeofday(&time, NULL);
-	return (time.tv_sec * 1000) + (time.tv_usec / 1000);
+	return ((time.tv_sec * 1000) + (time.tv_usec / 1000));
+}
+
+void	philo_print(const char *message, t_philo *philo)
+{
+	t_config			*config;
+	struct timeval		current_time;
+	unsigned long long	timestamp_in_ms;
+
+	config = philo->config;
+	gettimeofday(&current_time, NULL);
+	timestamp_in_ms = (current_time.tv_sec * 1000) + (current_time.tv_usec
+			/ 1000);
+	pthread_mutex_lock(&config->mutex_write);
+	ft_putnbr_fd(timestamp_in_ms, STDOUT_FILENO);
+	write(STDOUT_FILENO, " ", 1);
+	ft_putnbr_fd(philo->id, STDOUT_FILENO);
+	write(STDOUT_FILENO, " ", 1);
+	while (*message)
+	{
+		write(STDOUT_FILENO, message, 1);
+		message++;
+	}
+	write(STDOUT_FILENO, "\n", 1);
+	pthread_mutex_unlock(&config->mutex_write);
 }
